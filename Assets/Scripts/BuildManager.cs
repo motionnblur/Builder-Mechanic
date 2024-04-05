b31abe379d460a08aad2eec4eb24b75cf46df179
chill <carbonfiberdev@gmail.com>
2024-04-05 19:36:42 +0300

    add
    added box highlighting for placing on ground

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BuildManager : MonoBehaviour
 {
     [SerializeField] GameObject cube;
+    private GameObject objForHighlight;
     private bool _lock = true;
     private void OnEnable()
     {
@@ -15,15 +16,28 @@ public class BuildManager : MonoBehaviour
         EventManager.RemoveListener("OnMouseButtonDown", OnMouseButtonDown);
     }
 
-    private void OnRaycastHit(Vector3 hitPoing)
+    private void OnRaycastHit(Vector3 hitPoint)
     {
-        if (_lock) return;
+        HighLightObject(hitPoint);
 
-        Instantiate(cube, hitPoing, Quaternion.identity);
+        if (_lock) return;
+        Instantiate(cube, hitPoint, Quaternion.identity);
         _lock = true;
+    }
+    private void HighLightObject(Vector3 hitPoint)
+    {
+        if(!objForHighlight.activeSelf) objForHighlight.SetActive(true);
+        objForHighlight.transform.position = hitPoint;
     }
     private void OnMouseButtonDown()
     {
         if(Global.isOnCellDown) _lock = false;
+
+        if(objForHighlight == null)
+        {
+            objForHighlight = Instantiate(cube);
+            objForHighlight.SetActive(false);
+            objForHighlight.GetComponent<Renderer>().material.color = new Color(0.4428177f, 0.61199f, 0.8773585f, 0.5f);
+        }
     }
 }
